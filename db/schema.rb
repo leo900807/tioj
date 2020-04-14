b7f7524a8ccb0b1d525ef3b25fe0f6dd551b276d
leo900807 <leo900807@yahoo.com.tw>
2020-04-14 21:24:23 +0800

    Modify schema version to 20200309120400

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200118041340) do
+ActiveRecord::Schema.define(version: 20200309120400) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
